91ac04f80f1bff769616ff09037ad8edc8470ab8
Tomasz Elendt <tomasz.elendt@gmail.com>
2017-04-12 00:50:04 +0200

    Replace closures with structs

--- events.go
+++ events.go
@@ -5,20 +5,66 @@ import (
 	"fmt"
 )
 
+const (
+	followEventType       = 'F'
+	unfollowEventType     = 'U'
+	broadcastEventType    = 'B'
+	privateMsgEventType   = 'P'
+	statusUpdateEventType = 'S'
+)
+
 // ErrEventParseError is returned by ParseEvent to indicate that problems with
 // parsing event's payload.
 var ErrEventParseError = errors.New("Could not parse event's payload")
-
-type triggerFunc func(actions Actions)
-
-func (f triggerFunc) Trigger(actions Actions) {
-	f(actions)
-}
 
 // Event implements ActionsTrigger and has a sequence number
 type Event struct {
 	Seq int
 	ActionsTrigger
+}
+
+type followActionsTrigger struct {
+	followerID, followedID int
+	msg                    []byte
+}
+
+func (f followActionsTrigger) Trigger(actions Actions) {
+	actions.Follow(f.followerID, f.followedID)
+	actions.SendMsg(f.followedID, f.msg)
+}
+
+type unfollowActionsTrigger struct {
+	followerID, followedID int
+}
+
+func (u unfollowActionsTrigger) Trigger(actions Actions) {
+	actions.Unfollow(u.followerID, u.followedID)
+}
+
+type broadcastActionsTrigger struct {
+	msg []byte
+}
+
+func (b broadcastActionsTrigger) Trigger(actions Actions) {
+	actions.Broadcast(b.msg)
+}
+
+type privateMsgActionsTrigger struct {
+	userID int
+	msg    []byte
+}
+
+func (p privateMsgActionsTrigger) Trigger(actions Actions) {
+	actions.SendMsg(p.userID, p.msg)
+}
+
+type statusUpdateActionsTrigger struct {
+	userID int
+	msg    []byte
+}
+
+func (s statusUpdateActionsTrigger) Trigger(actions Actions) {
+	actions.SendMsgToFollowers(s.userID, s.msg)
 }
 
 // ParseEvent parses event's payload
@@ -34,28 +80,32 @@ func ParseEvent(payload []byte) (Event, error) {
 		byte
 		int
 	}
-	var trig triggerFunc
+	var trig ActionsTrigger
 	switch (tuple{evType[0], n}) {
-	case tuple{'F', 4}:
-		trig = func(actions Actions) {
-			actions.Follow(arg1, arg2)
-			actions.SendMsg(arg2, payload)
+	case tuple{followEventType, 4}:
+		trig = followActionsTrigger{
+			followerID: arg1,
+			followedID: arg2,
+			msg:        payload,
 		}
-	case tuple{'U', 4}:
-		trig = func(actions Actions) {
-			actions.Unfollow(arg1, arg2)
+	case tuple{unfollowEventType, 4}:
+		trig = unfollowActionsTrigger{
+			followerID: arg1,
+			followedID: arg2,
 		}
-	case tuple{'B', 2}:
-		trig = func(actions Actions) {
-			actions.Broadcast(payload)
+	case tuple{broadcastEventType, 2}:
+		trig = broadcastActionsTrigger{
+			msg: payload,
 		}
-	case tuple{'P', 4}:
-		trig = func(actions Actions) {
-			actions.SendMsg(arg2, payload)
+	case tuple{privateMsgEventType, 4}:
+		trig = privateMsgActionsTrigger{
+			userID: arg2,
+			msg:    payload,
 		}
-	case tuple{'S', 3}:
-		trig = func(actions Actions) {
-			actions.SendMsgToFollowers(arg1, payload)
+	case tuple{statusUpdateEventType, 3}:
+		trig = statusUpdateActionsTrigger{
+			userID: arg1,
+			msg:    payload,
 		}
 	default:
 		return event, ErrEventParseError
